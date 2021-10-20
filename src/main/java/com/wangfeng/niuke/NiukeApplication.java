50689086ed9d9c1d55fffb160a23dc0e299e1e54
wangfeng <2228952430@qq.com>
2021-10-20 23:14:00 +0800

    init

---
+++ src/main/java/com/wangfeng/niuke/NiukeApplication.java
@@ -0,0 +1,13 @@
+package com.wangfeng.niuke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiukeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiukeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangfeng/niuke/NiukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangfeng.niuke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiukeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
